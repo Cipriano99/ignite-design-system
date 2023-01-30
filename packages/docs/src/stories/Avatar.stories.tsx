5f302ef44d2f4cbe4a4811859eb00a55cc9b797e
Cipriano99 <samuelcipriano99@gmail.com>
2023-01-30 08:47:01 -0300

    Componentes do Design System - Componente: Avatar

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,21 @@
+import type { StoryObj, Meta } from "@storybook/react"
+import { Avatar, AvatarProps } from '@cipriano99-ignite-ui/react'
+
+export default {
+  title: 'Data display/Avatar',
+  component: Avatar,
+
+  args: {
+    src: 'https://github.com/cipriano99.png',
+    alt: 'Samuel Cipriano'
+  }
+
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined
+  }
+}
